575fede9ea701ee600925fb945d1e35a921fbdde
Ryan Swift <swiftryanc@gmail.com>
2025-08-16 13:32:00 -0400

    Added more debug to Player state update

--- music_assistant/providers/mpd/player.py
+++ music_assistant/providers/mpd/player.py
@@ -6,6 +6,7 @@ import asyncio
 import time
 from typing import TYPE_CHECKING
 
+from mpd import ConnectionError as MPDConnectionError
 from mpd.asyncio import MPDClient
 from music_assistant_models.enums import PlaybackState, PlayerFeature, PlayerType
 
@@ -38,7 +39,8 @@ class MusicPlayerDaemonPlayer(Player):
         self.ip_address = ip_address
         self.port = port
         # init some static variables
-        self._attr_name = f"MPD {player_id}"
+        self._attr_available = True
+        self._attr_name = f"{player_id} MPD"
         self._attr_type = PlayerType.PLAYER
         self._attr_supported_features = {
             PlayerFeature.VOLUME_SET,
@@ -48,7 +50,9 @@ class MusicPlayerDaemonPlayer(Player):
             PlayerFeature.ENQUEUE,
         }
         self._attr_device_info = DeviceInfo(
+            manufacturer="Music Player Daemon",
             ip_address=ip_address,
+            model="Unknown",
         )
         self._needs_poll = False
         self._mpd = MPDClient()
@@ -71,8 +75,15 @@ class MusicPlayerDaemonPlayer(Player):
 
     async def setup(self) -> None:
         """Set up the player."""
+        logger = self.provider.logger.getChild(self.player_id)
         # Establish the player connection
         await self._mpd.connect(self.ip_address, self.port)
+        self._attr_available = True
+        self._attr_device_info = DeviceInfo(
+            manufacturer="Music Player Daemon",
+            ip_address=self.ip_address,
+            model=self._mpd.mpd_version,
+        )
         # Clear any previous MPD playback and force settings to expected values
         await asyncio.gather(
             self._mpd.clear(),  # Clear the current player queue
@@ -83,9 +94,8 @@ class MusicPlayerDaemonPlayer(Player):
         )
         # Run the 'idle' function as an asyncio task to handle MPD Updates
         self.mass.create_task(self.idle())
-        self._attr_available = True
-        self.update_state()
-        logger = self.provider.logger.getChild(self.player_id)
+        # Get initial MPD Status
+        await self._set_attributes()
         logger.debug("Setup Complete")
 
     async def volume_set(self, volume_level: int) -> None:
@@ -171,7 +181,7 @@ class MusicPlayerDaemonPlayer(Player):
 
     async def poll(self) -> None:
         """Poll player for state updates."""
-        await self.get_mpd_status()
+        await self._set_attributes()
 
     async def on_unload(self) -> None:
         """Handle logic when the player is unloaded from the Player controller."""
@@ -179,23 +189,20 @@ class MusicPlayerDaemonPlayer(Player):
         self._mpd.disconnect()
         logger.info("Player %s unloaded", self.name)
 
-    async def get_mpd_status(self) -> None:
+    async def _set_attributes(self) -> None:
         """Update/set (dynamic) properties."""
         logger = self.provider.logger.getChild(self.player_id)
         if self._mpd.connected:
             self._attr_available = True
             status, song = await asyncio.gather(self._mpd.status(), self._mpd.currentsong())
-
             # State
-            if "state" in status:
-                self._attr_playback_state = PLAYBACK_STATE_MAP[status["state"]]
+            self._attr_playback_state = PLAYBACK_STATE_MAP.get(
+                status.get("state", "unknown"), PlaybackState.UNKNOWN
+            )
             logger.debug(f"PlaybackState : {self._attr_playback_state}")
-
             # Volume
-            if "volume" in status:
-                self._attr_volume_level = status["volume"]
+            self._attr_volume_level = status.get("volume", 100)
             logger.debug(f"VolumeLevel : {self._attr_volume_level}")
-
             # Elapsed Time
             if "elapsed" in status:
                 self._attr_elapsed_time = float(status["elapsed"])
@@ -205,7 +212,14 @@ class MusicPlayerDaemonPlayer(Player):
                 self._attr_elapsed_time = 0
             self._attr_elapsed_time_last_updated = time.time()
             logger.debug(f"ElapsedTime : {self._attr_elapsed_time}")
+            # Media Item
+            logger.debug(f"Song : {song}")
+            uri = song.get("file", None)
+            if uri and self._attr_current_media and uri != self._attr_current_media.uri:
+                self.set_current_media(uri=uri, clear_all=True)
+            logger.debug(f"MediaItem : {self._attr_current_media}")
         else:
+            logger.warning(f"Player {self.name} is Unavailable")
             self._attr_available = False
         self.update_state()
 
@@ -214,8 +228,10 @@ class MusicPlayerDaemonPlayer(Player):
         logger = self.provider.logger.getChild(self.player_id)
         try:
             async for subsystem in self._mpd.idle(["player", "playlist", "mixer", "options"]):
-                logger.debug(f"Idle Response: {subsystem}")
-                await self.get_mpd_status()
-        except Exception:
+                logger.debug(f"{','.join(subsystem)} updated")
+                await self._set_attributes()
+        except MPDConnectionError:
+            logger.warning(f"Connection to {self.name} lost")
+            self._attr_available = False
             self._attr_available = False
         self.update_state()
