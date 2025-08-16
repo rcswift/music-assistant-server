be245572e657e9cc4d80d988442aa8216de43718
Ryan Swift <swiftryanc@gmail.com>
2025-08-16 01:43:06 -0400

    Initial implementation of MPD

--- music_assistant/providers/mpd/__init__.py
+++ music_assistant/providers/mpd/__init__.py
@@ -1,34 +1,4 @@
-"""
-DEMO/TEST/DUMMY/TEMPLATE Player Provider for Music Assistant.
-
-This is an empty player provider with a test/demo implementation.
-Its meant to get started developing a new player provider for Music Assistant.
-
-Use it as a reference to discover what methods exists and what they should return.
-Also it is good to look at existing player providers to get a better understanding,
-due to the fact that providers may be flexible and support different features and/or
-ways to discover players on the network.
-
-In general, the actual device communication should reside in a separate library.
-You can then reference your library in the manifest in the requirements section,
-which is a list of (versioned!) python modules (pip syntax) that should be installed
-when the provider is selected by the user.
-
-To add a new player provider to Music Assistant, you need to create a new folder
-in the providers folder with the name of your provider (e.g. 'my_player_provider').
-In that folder you should create (at least) a __init__.py file and a manifest.json file.
-
-Optional is an icon.svg file that will be used as the icon for the provider in the UI,
-but we also support that you specify a material design icon in the manifest.json file.
-
-IMPORTANT NOTE:
-We strongly recommend developing on either macOS or Linux and start your development
-environment by running the setup.sh scripts in the scripts folder of the repository.
-This will create a virtual environment and install all dependencies needed for development.
-
-For all development instructions, please refer to the developer documentation:
-https://developers.music-assistant.io
-"""
+"""Music Player Daemon provider for Music Assistant."""
 
 from __future__ import annotations
 
@@ -37,8 +7,9 @@ from typing import TYPE_CHECKING
 from music_assistant_models.config_entries import ConfigEntry
 from music_assistant_models.enums import ConfigEntryType
 
-from .constants import CONF_NUMBER_OF_PLAYERS
-from .provider import DemoPlayerprovider
+from music_assistant.constants import CONF_IP_ADDRESS, CONF_PASSWORD, CONF_PORT
+
+from .provider import MPDPlayerProvider
 
 if TYPE_CHECKING:
     from music_assistant_models.config_entries import ConfigValueType, ProviderConfig
@@ -52,10 +23,7 @@ async def setup(
     mass: MusicAssistant, manifest: ProviderManifest, config: ProviderConfig
 ) -> ProviderInstanceType:
     """Initialize provider(instance) with given configuration."""
-    # setup is called when the user wants to setup a new provider instance.
-    # you are free to do any preflight checks here and but you must return
-    # an instance of your provider.
-    return DemoPlayerprovider(mass, manifest, config)
+    return MPDPlayerProvider(mass, manifest, config)
 
 
 async def get_config_entries(
@@ -72,20 +40,25 @@ async def get_config_entries(
     values: the (intermediate) raw values for config entries sent with the action.
     """
     # ruff: noqa: ARG001
-    # Config Entries are used to configure the Player Provider if needed.
-    # See the models of ConfigEntry and ConfigValueType for more information what is supported.
-    # The ConfigEntry is a dataclass that represents a single configuration entry.
-    # The ConfigValueType is an Enum that represents the type of value that
-    # can be stored in a ConfigEntry.
-    # If your provider does not need any configuration, you can return an empty tuple.
     return (
-        # example of a ConfigEntry for the number of players to create
         ConfigEntry(
-            key=CONF_NUMBER_OF_PLAYERS,
-            type=ConfigEntryType.INTEGER,
-            label="Number of Players",
+            key=CONF_IP_ADDRESS,
+            type=ConfigEntryType.STRING,
+            label="IP-Address (or hostname) of the device running mpd.",
             required=True,
-            default_value="2",
-            description="Number of demo players to create.",
+        ),
+        ConfigEntry(
+            key=CONF_PASSWORD,
+            type=ConfigEntryType.SECURE_STRING,
+            label="Password to use to connect to mpd.",
+            required=False,
+        ),
+        ConfigEntry(
+            key=CONF_PORT,
+            type=ConfigEntryType.INTEGER,
+            default_value=6600,
+            label="Port to use to connect to the mpd (default is 6600).",
+            required=True,
+            category="advanced",
         ),
     )
--- music_assistant/providers/mpd/constants.py
+++
@@ -1,4 +0,0 @@
-"""Constants for the Demo Player Provider."""
-
-CONF_NUMBER_OF_PLAYERS = "number_of_players"
-CONF_PLAYER_NAME_PREFIX = "player_name_prefix"
