8e4c401de3d2ca0a42a327fc5ddb6927eb302640
Ryan Swift <swiftryanc@gmail.com>
2025-08-16 13:33:08 -0400

    Removed demo player comments and docstrings

--- music_assistant/providers/mpd/provider.py
+++ music_assistant/providers/mpd/provider.py
@@ -20,21 +20,7 @@ if TYPE_CHECKING:
 
 
 class MusicPlayerDaemonPlayerProvider(PlayerProvider):
-    """
-    Example/demo Player provider.
-
-    Note that this is always subclassed from PlayerProvider,
-    which in turn is a subclass of the generic Provider model.
-
-    The base implementation already takes care of some convenience methods,
-    such as the mass object and the logger. Take a look at the base class
-    for more information on what is available.
-
-    Just like with any other subclass, make sure that if you override
-    any of the default methods (such as __init__), you call the super() method.
-    In most cases its not needed to override any of the builtin methods and you only
-    implement the abc methods with your actual implementation.
-    """
+    """Music Player Daemon Player Provider."""
 
     async def handle_async_init(self) -> None:
         """Handle async initialization of the provider."""
@@ -53,40 +39,14 @@ class MusicPlayerDaemonPlayerProvider(PlayerProvider):
         Called when provider is deregistered (e.g. MA exiting or config reloading).
         is_removed will be set to True when the provider is removed from the configuration.
         """
-        # OPTIONAL
-        # this is an optional method that you can implement if
-        # relevant or leave out completely if not needed.
-        # it will be called when the provider is unloaded from Music Assistant.
-        # this means also when the provider is getting reloaded
         for player in self.players:
-            # if you have any cleanup logic for the players, you can do that here.
-            # e.g. disconnecting from the player, closing connections, etc.
             self.logger.debug("Unloading player %s", player.name)
             await self.mass.players.unregister(player.player_id)
-
-    def on_player_enabled(self, player_id: str) -> None:
-        """Call (by config manager) when a player gets enabled."""
-        # OPTIONAL
-        # this is an optional method that you can implement if
-        # you want to do something special when a player is enabled.
-
-    def on_player_disabled(self, player_id: str) -> None:
-        """Call (by config manager) when a player gets disabled."""
-        # OPTIONAL
-        # this is an optional method that you can implement if
-        # you want to do something special when a player is disabled.
-        # e.g. you can stop polling the player or disconnect from it.
-
-    async def remove_player(self, player_id: str) -> None:
-        """Remove a player from this provider."""
-        # OPTIONAL - required only if you specified ProviderFeature.REMOVE_PLAYER
-        # this is used to actually remove a player.
 
     async def on_mdns_service_state_change(
         self, name: str, state_change: ServiceStateChange, info: AsyncServiceInfo | None
     ) -> None:
         """Handle MDNS service state callback."""
-        # MANDATORY IF YOU WANT TO USE MDNS DISCOVERY
         if not info:
             return  # guard
 
